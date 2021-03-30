4e87f42b7860f0bab8ef3756833f8bbd81418ef1
CD <codedecode.dev@gmail.com>
2021-03-30 15:45:04 +0530

    Renamed Main.tscn to Default.tscn

---
+++ Server/app.py
@@ -0,0 +1,69 @@
+import socket
+import threading
+
+
+
+# Connection Data
+HOST = '127.0.0.1'
+PORT = 6969
+
+
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
+
+
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
+
+
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+
+
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Print And Broadcast Nickname
+        print(f"{address} Connected as {nickname}")
+        broadcast("{} joined!\n".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('utf-8'))
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server running...")
+receive()
